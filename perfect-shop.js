f80222e0f30e3760b26b52d76029ae1d29c65f91
Zach Oran <69660837+zachoran@users.noreply.github.com>
2020-12-15 19:56:32 -0500

    added persistent data and remove button

--- perfect-shop.js
+++ perfect-shop.js
@@ -1,7 +1,7 @@
-const krogerItemList = []
-const targetItemList = []
-const foodCityItemList = []
-const costcoItemList = []
+let krogerItemList = []
+let targetItemList = []
+let foodCityItemList = []
+let costcoItemList = []
 let selectedStoreButton = ''
 
 document.querySelector('#kroger-button').addEventListener('click', function () {
@@ -20,20 +20,18 @@ document.querySelector('#costco-button').addEventListener('click', function () {
     selectedStoreButton = 'Costco'
 })
 
-
 document.querySelector('#add-item-button').addEventListener('click', function () {
     const userInput = document.querySelector('#item-input-blank').value
     if (userInput !== '') {
         if (selectedStoreButton === 'Kroger') {
-            krogerItemList.push(userInput)
+            krogerItemList.push({title: userInput, completed: false})
         } else if (selectedStoreButton === 'Target') {
-            targetItemList.push(userInput)
+            targetItemList.push({title: userInput, completed: false})
         } else if (selectedStoreButton === 'Food City') {
-            foodCityItemList.push(userInput)
+            foodCityItemList.push({title: userInput, completed: false})
         } else if (selectedStoreButton === 'Costco') {
-            costcoItemList.push(userInput)
+            costcoItemList.push({title: userInput, completed: false})
         }
-
     }
     document.querySelector('#item-input-blank').value = ''
     renderItems()
@@ -42,28 +40,30 @@ document.querySelector('#add-item-button').addEventListener('click', function ()
 const renderItems = function () {
     document.querySelector('#kroger-item-list').innerHTML = ''
     krogerItemList.forEach(function (currentItem) {
-        addItems(currentItem, '#kroger-item-list')
+        renderListItem(currentItem, '#kroger-item-list')
     })
     document.querySelector('#target-item-list').innerHTML = ''
     targetItemList.forEach(function (currentItem) {
-        addItems(currentItem, '#target-item-list')
+        renderListItem(currentItem, '#target-item-list')
     })
     document.querySelector('#food-city-item-list').innerHTML = ''
     foodCityItemList.forEach(function (currentItem) {
-        addItems(currentItem, '#food-city-item-list')
+        renderListItem(currentItem, '#food-city-item-list')
     })
     document.querySelector('#costco-item-list').innerHTML = ''
     costcoItemList.forEach(function (currentItem) {
-        addItems(currentItem, '#costco-item-list')
+        renderListItem(currentItem, '#costco-item-list')
     })
 }
 
-const addItems = function (currentItem, store) {
+const renderListItem = function (currentItem, store) {
     const listItem = document.createElement('li')
-    listItem.textContent = currentItem
+    listItem.textContent = currentItem.title
     listItem.classList.add('listItem')
     document.querySelector(store).appendChild(listItem)
     makeCheckbox(listItem)
+    makeRemoveButton(listItem, 0, store)
+    saveItems()
 }
 
 const makeCheckbox = function (listItem) {
@@ -76,4 +76,53 @@ const makeCheckbox = function (listItem) {
             checkbox
         }
     })
+    saveItems()
 }
+
+const makeRemoveButton = function (listItem, itemIndex, store) {
+    const removeButton = document.createElement('button')
+    removeButton.innerHTML = 'Remove'
+    removeButton.classList.add('remove-button')
+    listItem.appendChild(removeButton)
+    removeButton.addEventListener('click', function () {
+
+        // these are the html id's
+
+        if (store === '#kroger-item-list') {
+            krogerItemList.splice(itemIndex, 1)
+        } else if (store === '#target-item-list') {
+            targetItemList.splice(itemIndex, 1)
+        } else if (store === '#food-city-item-list') {
+            foodCityItemList.splice(itemIndex, 1)
+        } else {
+            costcoItemList.splice(itemIndex, 1)
+        }
+        renderItems()
+        saveItems()                   
+    })
+}
+
+const loadItems = function () {
+    const krogerItemListJSON = localStorage.getItem('perfect-shop-kroger-list')
+    krogerItemList = JSON.parse(krogerItemListJSON)
+    const targetItemListJSON = localStorage.getItem('perfect-shop-target-list')
+    targetItemList = JSON.parse(targetItemListJSON)
+    const foodCityItemListJSON = localStorage.getItem('perfect-shop-food-city-list')
+    foodCityItemList = JSON.parse(foodCityItemListJSON)
+    const costcoItemListJSON = localStorage.getItem('perfect-shop-costco-list')
+    costcoItemList = JSON.parse(costcoItemListJSON)
+}
+
+const saveItems = function () {
+    localStorage.setItem('perfect-shop-kroger-list', JSON.stringify(krogerItemList))
+    localStorage.setItem('perfect-shop-target-list', JSON.stringify(targetItemList))
+    localStorage.setItem('perfect-shop-food-city-list', JSON.stringify(foodCityItemList))
+    localStorage.setItem('perfect-shop-costco-list', JSON.stringify(costcoItemList))
+}
+
+const init = function () {
+    loadItems()
+    renderItems()
+}
+
+init()
